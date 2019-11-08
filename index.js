1c98d1cf05f98143fef67ecc2edc88cae86ba0ea
shuaibcs <alshuaibi7@gmail.com>
2019-11-08 10:28:56 +0100

    setup4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const app = express()
 const db = require('./db')
+const Image = require('./image/model')
 const port = process.env.PORT || 4000
 app.listen(port, console.log(`Listening on ${port}`));
