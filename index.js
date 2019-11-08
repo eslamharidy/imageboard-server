e4123e0681b7594bea54094924f3bf7f0ec5d6ac
shuaibcs <alshuaibi7@gmail.com>
2019-11-08 13:26:14 +0100

    signin-signup

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const db = require('./db')
 const Image = require('./image/model')
 const imageRouter = require('./image/router')
+const JwtRouter = require('./auth/router')
+const userRouter = require('./user/router')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const corsMiddleware = cors()
@@ -10,7 +12,8 @@ app.use(corsMiddleware)
 
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
-
+app.use(JwtRouter)
+app.use(userRouter)
 app.use(imageRouter)
 const port = process.env.PORT || 4000
 app.listen(port, console.log(`Listening on ${port}`));
