543aa245441bbc2bb24586c1de081855d3d17c71
shuaibcs <alshuaibi7@gmail.com>
2019-11-08 16:53:18 +0100

    front-end-done

--- image/router.js
+++ image/router.js
@@ -1,21 +1,24 @@
 const {Router} = require('express')
 const Image = require('./model')
 const router = new Router()
+const auth = require ('../auth/middleware')
 
 
 //Get all images
 router.get('/image', (req, res, next) => {
-    const limit = req.query.limit || 5
+    const limit = req.query.limit || 20
     const offset = req.query.offset || 0
   
-    Image.findAndCountAll({ limit, offset })
+    Image.findAndCountAll({ limit, offset, order: [
+      ['updatedAt', 'DESC']
+  ] })
       .then(result => res.send({ images: result.rows, total: result.count }))
       .catch(error => next(error))
   })
 
 
   //add image
-router.post('/image', (req,res,next) => {
+router.post('/image',auth, (req,res,next) => {
     Image.create(req.body)
     .then(image => res.send(image))
     .catch(next)
